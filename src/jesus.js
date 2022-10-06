ea6d330cc2a9d9df112e160e26aaf2bf6c1d8c2b
8jesuspinniusharani <pinni.usha44@gmail.com>
2022-10-06 23:26:51 +0530

    first commit

---
+++ src/jesus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+const Jesus = () => {
+  return (
+    <div className='box'>
+        <h1>I love jesus</h1>
+      
+    </div>
+  )
+}
+
+export default Jesus
